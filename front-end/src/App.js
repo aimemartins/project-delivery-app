9863db9229fea42c8960fde4efeae027513b2a38
Joao Fabris <jvfabris21@gmail.com>
2023-05-04 19:16:26 -0300

    Req 01 ok

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
       <Route path="/login" component={ Login } />
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
     </Switch>
   );
 }
