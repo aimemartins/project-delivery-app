c6472d30d37377a82ed2884e1b6301098f87ccc4
Gabriel Lemes <rlemes.gabriel@gmail.com>
2023-05-08 15:39:06 -0300

    feat: função acessControl para o backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,8 +6,16 @@ const {
   SaleProductController } = require('../database/controller');
 const ErrorHandler = require('../database/middlewares/ErrorHandler');
 
+const accessControl = (_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+
 const app = express();
 app.use(express.json());
+app.use(accessControl);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
