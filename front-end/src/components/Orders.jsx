d6a211847f362f706b9c9343aa91170a3c10398c
Gabriel Lemes <rlemes.gabriel@gmail.com>
2023-05-17 15:51:23 -0300

    Realização da tela 05

--- front-end/src/components/Orders.jsx
+++ front-end/src/components/Orders.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Orders({ id, status, saleDate, totalPrice, deliveryAddress }) {
   const [user, setUser] = useState({});
+  const newDate = new Intl.DateTimeFormat('pt-BR').format(new Date(saleDate));
 
   useEffect(() => {
     const objUser = JSON.parse(localStorage.getItem('user'));
@@ -10,28 +12,50 @@ export default function Orders({ id, status, saleDate, totalPrice, deliveryAddre
   }, []);
   if (user.role === 'customer') {
     return (
-      <div>
-        <h2 data-testid={ `customer_orders__element-order-id-${id}` }>{id}</h2>
-        <h2 data-testid={ `customer_orders__element-delivery-status-${id}` }>{status}</h2>
-        <br />
-        <h2 data-testid={ `customer_orders__element-order-date-${id}` }>{saleDate}</h2>
-      </div>
+      <Link
+        to={ `/customer/orders/${id}` }
+      >
+        <div>
+          <h2 data-testid={ `customer_orders__element-order-id-${id}` }>{id}</h2>
+          <h2
+            data-testid={ `customer_orders__element-delivery-status-${id}` }
+          >
+            {status}
+          </h2>
+          <br />
+          <h2 data-testid={ `customer_orders__element-order-date-${id}` }>{newDate}</h2>
+          <h2
+            data-testid={ `customer_orders__element-card-price-${id}` }
+          >
+            {totalPrice.replace('.', ',')}
+          </h2>
+        </div>
+      </Link>
     );
   }
   if (user.role === 'seller') {
     return (
-      <div>
-        <h2 data-testid={ `seller_orders__element-order-id-${id}` }>{id}</h2>
-        <h2 data-testid={ `seller_orders__element-delivery-status-${id}` }>{status}</h2>
-        <br />
-        <h2 data-testid={ `seller_orders__element-order-date-${id}` }>{saleDate}</h2>
-        <h2 data-testid={ `seller_orders__element-card-price-${id}` }>{totalPrice}</h2>
-        <h2 data-testid={ `seller_orders__element-card-address-${id}` }>
-          {deliveryAddress}
-        </h2>
-      </div>
+      <Link
+        to={ `/seller/orders/${id}` }
+      >
+        <div>
+          <h2 data-testid={ `seller_orders__element-order-id-${id}` }>{id}</h2>
+          <h2 data-testid={ `seller_orders__element-delivery-status-${id}` }>{status}</h2>
+          <br />
+          <h2 data-testid={ `seller_orders__element-order-date-${id}` }>{newDate}</h2>
+          <h2 data-testid={ `seller_orders__element-card-price-${id}` }>
+            {totalPrice.replace('.', ',')}
+          </h2>
+          <h2 data-testid={ `seller_orders__element-card-address-${id}` }>
+            {deliveryAddress}
+          </h2>
+        </div>
+      </Link>
     );
   }
+  return (
+    <p>Loading</p>
+  );
 }
 
 Orders.defaultProps = {
@@ -42,6 +66,6 @@ Orders.propTypes = {
   id: PropTypes.number.isRequired,
   status: PropTypes.string.isRequired,
   saleDate: PropTypes.string.isRequired,
-  totalPrice: PropTypes.number.isRequired,
+  totalPrice: PropTypes.string.isRequired,
   deliveryAddress: PropTypes.string,
 };
